fe43cb5fe245317f848ab0dba940760c35bbb426
iamiamn <chestnutljy@outlook.com>
2019-10-14 21:13:16 -0500

    add test2 in controlled tests

--- test/controlled_tests.py
+++ test/controlled_tests.py
@@ -9,3 +9,19 @@ class ControlledTests(unittest.TestCase):
         computed_result = compute_highest_affinity.highest_affinity(site_list, user_list, time_list)
         expected_result = ("a.com", "b.com")
         self.assertEqual(computed_result, expected_result)
+    def test2(self):
+        num_lines = 10000
+        num_user
+        s = 1000
+
+        site_list = ["a.com", "b.com", "a.com", "b.com", "a.com", "c.com"]
+        user_list = ["andy", "andy", "bob", "bob", "charlie", "charlie"]
+        time_list = range(0, num_lines)
+
+        computed_result = compute_highest_affinity.highest_affinity(
+            site_list, user_list, time_list)
+        expected_result = ("facebook", "google")
+
+        assert computed_result == expected_result
+        print(
+            "Successfully passed {}!".format(os.path.basename(__file__).split(".")[0]))
